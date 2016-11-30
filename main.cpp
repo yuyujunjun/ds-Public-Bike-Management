1b2921968293c25c511f08323f30bf6a774f72ef
yunuohanxue <fcsx1tf@163.com>
2016-11-30 21:33:50 +0800

    find the min path

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include<stdlib.h>
+#define MaxVNodeNum 500
+#define INFINITY 100000
 typedef struct AdjVNode *edge;
 typedef int Vertex;
+struct MinPath{
+    int sent;
+    int back;
+    int path[MaxVNodeNum];
+};
 typedef struct Vnode{
     int number;
     int known;
+    int dist;
+    int path[MaxVNodeNum];
+    int pathIndex;
     edge FirstEdge;
 }*AdjList;
 struct AdjVNode{
@@ -23,10 +33,12 @@ struct GNode{
 };
 typedef PtrToGNode LGraph;
 LGraph create(int *Sp);
+void ShortestDist( LGraph graph );
 void Print(LGraph graph);
 int main() {
     int Sp;
     LGraph graph=create(&Sp);
+    ShortestDist(graph);
     Print(graph);
     return 0;
 }
@@ -41,11 +53,14 @@ LGraph create(int *Sp)
     graph->G[0].FirstEdge=NULL;
     graph->G[0].number=1000;
     graph->G[0].known=0;
+    graph->G[0].dist=0;
     for(i=1;i<=graph->Nv;i++)
     {
         scanf("%d",&graph->G[i].number);
         graph->G[i].FirstEdge=NULL;
         graph->G[i].known=0;
+        graph->G[i].dist=INFINITY;
+        for(int j=0;j<graph->Nv;j++)graph->G[i].path[j]=-1;
     }
     for(i=0;i<graph->Ne;i++)
     {
@@ -95,6 +110,44 @@ void Print(LGraph graph)
         {
             printf("next edge:%d,time:%d \n",temp->AdjV,temp->weighted);
             temp=temp->Next;
+        }
+    }
+}
+void ShortestDist( LGraph graph )
+{
+    int i;
+    while(1)
+    {
+        int MinDist=INFINITY-1;
+        int index;
+        int flag=0;
+        for(i=0;i<graph->Nv;i++)
+        {
+            if(graph->G[i].dist<MinDist&&graph->G[i].known==0)
+            {
+                MinDist=graph->G[i].dist;
+                index=i;
+                flag=1;
+            }
+        }
+        if(flag==0)break;
+        graph->G[index].known=1;
+        edge temp=graph->G[index].FirstEdge;
+        for(;temp;temp=temp->Next)
+        {
+            if(!(graph->G[temp->AdjV].known))
+            {
+                if(graph->G[index].dist+temp->weighted<graph->G[temp->AdjV].dist)
+                {
+                    graph->G[temp->AdjV].dist=graph->G[index].dist+temp->weighted;
+                    graph->G[temp->AdjV].pathIndex=0;
+                }else if(graph->G[index].dist+temp->weighted==graph->G[temp->AdjV].dist)
+                {
+                    graph->G[temp->AdjV].pathIndex++;
+
+                }
+                graph->G[temp->AdjV].path[graph->G[temp->AdjV].pathIndex]=index;
+            }
         }
     }
 }
